9d8722ce449effb54615f1897b25c86dba415bd2
Tim Lee <leewtim@gmail.com>
2018-09-09 23:20:43 -0700

    Finished MongoDB and Graph QL. Added React

--- server/index.js
+++ server/index.js
@@ -11,6 +11,7 @@ const Todo = mongoose.model('Todo', {
 const typeDefs = `
   type Query {
     hello(name: String): String!
+    todos: [Todo]
   }
   type Todo {
     id: ID
@@ -19,19 +20,30 @@ const typeDefs = `
   }
   type Mutation {
     createTodo(text: String!): Todo
+    updateTodo(id: ID!, complete: Boolean!): Boolean
+    removeTodo(id: ID!): Boolean
   }
 `;
 
 const resolvers = {
   Query: {
-    hello: (_, { name }) => `Hello ${name || "World"}`
+    hello: (_, { name }) => `Hello ${name || "World"}`,
+    todos: () => Todo.find()
   },
   Mutation: {
     createTodo: async (_, { text }) => {
       const todo = new Todo({ text, complete: false });
       await todo.save();
       return todo;
-    }
+    },
+    updateTodo: async (_, { id, complete }) => {
+      await Todo.findByIdAndUpdate(id, {complete});
+      return true;
+    },
+    removeTodo: async (_, { id }) => {
+      await Todo.findByIdAndRemove(id);
+      return true;
+    },
   }
 };
 
